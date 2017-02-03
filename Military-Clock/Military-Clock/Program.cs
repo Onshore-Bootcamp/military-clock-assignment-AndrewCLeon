0468a8dc324be14e68c613a2ca283f3f126673a5
AndrewCLeon <andrew.c.leon.18@gmail.com>
2017-02-03 11:48:11 -0600

    Add files via upload

---
+++ Military-Clock/Military-Clock/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Requirements
+ 
+    Task - Create a military style clock using C#.
+
+    Requirements - The clock must be a 24 hour clock. Format:{ 23:59:59 }
+                 - The clock must tick at 1 second per second.
+                 - The clock is not required to display the current time.
+                 
+    Forbidden    - You may not use DateTime.Now or any DateTime methods.
+
+    Notes        - If you write it twice, it should be a method.
+                 - If you can make it dynamic, you might as well.
+ */
+
+namespace Military_Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
